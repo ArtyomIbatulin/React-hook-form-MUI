9646aa0dfd861cbe3427a8c2aab08421261d289e
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2023-12-31 19:33:23 +0300

    added authPage & install mui

---
+++ src/pages/auth-page.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import Typography from "@mui/material/Typography";
+
+export const AuthPage: FC = () => {
+  return (
+    <Typography variant="h4" gutterBottom>
+      h4. Heading
+    </Typography>
+  );
+};
